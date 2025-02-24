3ece43d078bdd312405dfc51e24fda15e20b8a14
Shivam Soni <shivam.soni1998@gmail.com>
2025-02-25 00:41:51 +0530

    frontend initial commit

--- server/index.js
+++ server/index.js
@@ -3,25 +3,52 @@ const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const axios=require('axios');
+const axios = require("axios");
 async function startServer() {
   const app = express();
   const server = new ApolloServer({
     typeDefs: `
+  type User {
+        id:ID!
+        name:String!
+          username:String!
+            email:String!
+              phone:String!
+              website:String!
+       
+        }
+
         type Todo {
         id:ID!
         title:String!
         completed:Boolean
+        user:User
         }
 
         type Query {
         getTodos:[Todo]
+        getAllUsers:[User]
+        getUser(id:ID!):User
         }
         `,
     resolvers: {
-        Query:{
-            getTodos:async()=>(await axios.get('https://jsonplaceholder.typicode.com/todos')).data
-        }
+      Todo: {
+        user: async (todo) =>
+          (
+            await axios.get(
+              `https://jsonplaceholder.typicode.com/users/${todo.userId}`
+            )
+          ).data,
+      },
+      Query: {
+        getTodos: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/todos")).data,
+        getAllUsers: async () =>
+          (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+        getUser: async (parent, { id }) =>
+          (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+            .data,
+      },
     },
   });
 
