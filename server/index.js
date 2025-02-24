c2198f975860a3865c837c50cda47bbb87dbea36
Shivam Soni <shivam.soni1998@gmail.com>
2025-02-24 23:45:30 +0530

    type defs

--- server/index.js
+++ server/index.js
@@ -1,5 +1,36 @@
-const express=require('express');
-const {ApolloServer}=require('@apollo/server');
-const {expressMiddleware}=require("@apollo/server/express4")
-const bodyParser=require('body-parser');
-const cors=require("cors")
+const express = require("express");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios=require('axios');
+async function startServer() {
+  const app = express();
+  const server = new ApolloServer({
+    typeDefs: `
+        type Todo {
+        id:ID!
+        title:String!
+        completed:Boolean
+        }
+
+        type Query {
+        getTodos:[Todo]
+        }
+        `,
+    resolvers: {
+        Query:{
+            getTodos:async()=>(await axios.get('https://jsonplaceholder.typicode.com/todos')).data
+        }
+    },
+  });
+
+  app.use(bodyParser.json());
+  app.use(cors());
+
+  await server.start();
+  app.use("/graphql", expressMiddleware(server));
+  app.listen(8000, () => console.log("server started at PORT 8000"));
+}
+
+startServer();
